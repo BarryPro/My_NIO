216f6d0582941f62ecbd93db2586c33f44e578c4
belong <belong.belong@outlook.com>
2017-04-19 11:02:03 +0800

    修改NIO的内容 ============ 添加只读缓冲区 ============ 新添加JavaFX的界面项目

--- src/com/belong/nio/AllocateDirect.java
+++ src/com/belong/nio/AllocateDirect.java
@@ -12,11 +12,11 @@ import java.nio.channels.FileChannel;
  */
 public class AllocateDirect {
     static public void main(String args[]) throws Exception {
-        String infile = "c:\\test.txt";
+        String infile = "D:\\logs\\tmp.txt";
         FileInputStream fin = new FileInputStream(infile);
         FileChannel fcin = fin.getChannel();
 
-        String outfile = String.format("c:\\testcopy.txt");
+        String outfile = String.format("D:\\logs\\belong3.log");
         FileOutputStream fout = new FileOutputStream(outfile);
         FileChannel fcout = fout.getChannel();
 
@@ -24,15 +24,20 @@ public class AllocateDirect {
         ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
 
         while (true) {
+            // 先清空缓存
             buffer.clear();
-
+            // 把信息从通道读出放到缓冲区
             int r = fcin.read(buffer);
-
+            // 当通道中没有数据就会返回-1
             if (r == -1) {
                 break;
             }
+            // 反转缓冲区
             buffer.flip();
+            // 把缓冲区的内容写到输出通道
             fcout.write(buffer);
         }
+        fcout.close();
+        fcin.close();
     }
 }
